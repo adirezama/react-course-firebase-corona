f0f25040f5aedb7f8e34d42fd3bfc85641b152fe
adirezama <71096093+adirezama@users.noreply.github.com>
2020-11-13 01:46:28 +0700

    minor-fix

--- src/components/coronaNews/index.js
+++ src/components/coronaNews/index.js
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from 'react';
 // import moment from 'moment';
+import './style.css';
 import app from '../../services/firebase';
 import 'firebase/database';
 
-const convertTanggalKeLocale = (tanggal) => {
-  const tanggalLokal = new Date(tanggal).toLocaleString('id-ID', {
+const localeTime = (tanggal) => {
+  const localeDate = new Date(tanggal).toLocaleString('id-ID', {
     dateStyle: 'full',
   });
-  return tanggalLokal;
+  return localeDate;
 };
 
 const CoronaNews = () => {
@@ -26,41 +27,67 @@ const CoronaNews = () => {
 
   return (
     <div>
-      <h2>data corona</h2>
       {isLoading ? <p>loading</p> : <p />}
-      <div className="corona__news-wrapper">
-        {news.map((newsItem) => {
-          return (
-            <div>
-              <h1 style={{ marginTop: '2em' }}>
-                {convertTanggalKeLocale(newsItem.date)}
-              </h1>
-              <div className="news-wrapper">
-                {newsItem.activity.map((activityItem) => {
-                  return (
-                    <ul className="container_status">
-                      <a
-                        href="href={activityItem.url}"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <strong className="container_title">
-                          {activityItem.title}
-                        </strong>
-                      </a>
-
-                      <p className="news-description">{activityItem.desc}</p>
-                    </ul>
-                  );
-                })}
+      <div>
+        <div class="corona__news-wrapper">
+          {news.map((newsItem) => {
+            return (
+              <div className="container">
+                <h1>{localeTime(newsItem.date)}</h1>
+                <div className="news-wrapper">
+                  {newsItem.activity.map((activityItem) => {
+                    return (
+                      <div class="container_status">
+                        <a
+                          href={activityItem.url}
+                          target="_blank"
+                          rel="noreferrer"
+                        >
+                          <ul>
+                            <li>
+                              <h2 className="news-title">
+                                {activityItem.title}
+                              </h2>
+                              <p className="news-description">
+                                {activityItem.desc}
+                              </p>
+                            </li>
+                          </ul>
+                        </a>
+                      </div>
+                    );
+                  })}
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
     </div>
   );
 
+  // return (
+  //   <div>
+  //     <h2>{localeTime(newsItem.date)}</h2>
+  //     <div class="news-wrapper">
+  //       {newsItem.activity.map((activityItem) => {
+  //         return (
+  //           <div class="container_status">
+  //             <a
+  //               href="href={activityItem.url}"
+  //               target="_blank"
+  //               rel="noopener noreferrer"
+  //             >
+  //               <strong class="container_title">{activityItem.title}</strong>
+  //             </a>
+
+  //             <p class="news-description">{activityItem.desc}</p>
+  //           </div>
+  //         );
+  //       })}
+  //     </div>
+  //   </div>
+  // );
   // return (
   //   <div>
   //     <h2>data corona</h2>
